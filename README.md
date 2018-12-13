d94a821136f8551d7a1948ba89f804bf73782e4f
chiyoko2 <chiyoko2@illinois.edu>
2018-12-13 01:42:00 -0800

    further refinement

--- README.md
+++ README.md
@@ -1,12 +1,25 @@
+A Classifier project - Find self-proclaimed authenticity experts in restaurant reviews
 
-A Classfier project - Find self-proclaimed authenticity expert in restaurant reviews
+    This classifier determines if a review text is written by
+    someone who has authentic cuisine experiences by living in the country or from the country.
 
-Steps and approach to this project
+    Although there are some hooks to handle different cuisines,
+    for the initial phase, reviews for Japanese restaurants are used to do this classification.
+    The classifier trained in reviews from a Japanese restaurant is looking
+    for word pattern matches along with Entity Recognition tag. Phrases such
+    as "lived in Japan" or "I am Japanese" should have a high probability for
+    authenticity experts' reviews.
 
-1. Determine features and how to encode those features
+***How to run the script***
 
-2. create a feature extractor fucntion to return a feature set
-a feature set = maps from feature's names to their values.
-a featuer value = values with simple types such as Booleans, numbers, and strings
+GitLab:
+Using the .gitlab-ci.yml in this repository, you can run this in your gitlab environment by "run pipeline".
 
-3. prepare a list of examples and corresponding class labels:
+
+GitHub:
+If you download this repository to your environment, then you can run the script by: 
+
+
+pip install --upgrade nltk numpy spacy
+python -m spacy download en
+python cuisineExpClassify.py
--- cuisineExpClassify.py
+++ cuisineExpClassify.py
@@ -1,3 +1,4 @@
+import sys
 import nltk
 import re
 import spacy
@@ -5,37 +6,106 @@ import en_core_web_sm
 import csv
 import random
 
+#Constants Defined 
+EXPERT = "EXPT"
+NOEXPERT = "NOEX"
+DEFAULT_CUISINE = "japanese"
 
+"""
+Global Variables
+These should be loaded up from some corpus.
+For this project, define them here.
+"""
+
+# Lookup Table for country and naionality
 country_dict = {} 
+
+# Lookup Table for each country and its prominent cities
 geo_words = {}
+
 def load_country():
+    """
+    The following function loads up the country and nationality lookup table into a dictionary 
+    """
     with open('./data/my_country_national.csv', 'rU', newline='', encoding='utf-8') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='"')
         for row in reader:
             country_dict[row[1].lower()] = row[0].lower()
 
 def load_geo_words():
+    """
+    The following function defines the lookup table for each country and its cities  into a dictionary. 
+    For this project, just work with 'japanese' only
+    """
+
     geo_words['japanese'] = ('japan', 'japanese', 'tokyo', 'osaka', 'yokohama', 'okinawa', 'kyoto', 'nara', 'harajuku', 'shibuya','ginza') 
+    geo_words['chinese'] = ('china', 'beijing', 'shanghai', 'hong kong') 
 
 def entity_features(raw, cuisine_type):
+    """
+    The following function defines the feature extraction for this classifier. 
+    
+    Use spaCy to do NLP using en_core_web_sm. It gives us easy token interface in moving around and checking the token types.
+    NOTE: I took out token.pos_ checking as it was making this function overfitting.
+    
+    Check the entity type to see if the token contains a country or nationality.  
+    If found, we are interested in 2 tokens left of the token to obeserve the patters such as:
+    GPE -> VERB/ADP/GPE (e.g. live in Japan)
+    NORP -> /PRON/VERB/GPE (e.g. I am Japanese)
+    """
+
     nlp = en_core_web_sm.load()
     doc = nlp(raw)
     features={}
     for token in doc:
-        if token.ent_type_ == 'GPE':
-            if token.i > 1 and token.text.lower() in geo_words[cuisine_type] and doc[token.i - 1].pos_ == 'ADP' and doc[token.i-2].pos_=='VERB':
-                features['contains({})'.format(doc[token.i-2].text + " "+ doc[token.i -1].text + " " + token.text)] = True
-        elif token.ent_type_ == 'NORP':
-            if token.i > 1 and token.text.lower() in geo_words[cuisine_type] and (doc[token.i - 1].pos_ == "VERB" and doc[token.i-2].pos_ == "PRON") and (doc[token.i -
-            1].text.lower() in ("'re", "are", "'m", "am") and doc[token.i-2].text.lower() in ("i", "we")):
-                features['contains({})'.format(doc[token.i-2].text + " "+ doc[token.i -1].text + " " + token.text)] = True
-        else:
-            features['contains({})'.format(token.text)] = False 
+        if token.i > 1 and token.ent_type_ in ('GPE', 'NORP') and token.text.lower() in geo_words[cuisine_type]:
+            features['contains({})'.format(doc[token.i-2].text + " "+ doc[token.i -1].text + " " + token.text)] = token.ent_type_
     return features
 
+
+def classify_this(classifier, review_text, restaurant_category):
+    """
+    The following function calls the classifier to classify the text and print out the result 
+    """
+    found_features = {}
+    found_features=entity_features(review_text, restaurant_category)
+    if (classifier.classify(found_features) == EXPERT):
+        print ("Authenticity Expoert's Review")
+    else:
+        print ("Non-authenticity Expoert's Review")
+
+def classify_with_input(classifier, restaurant_category):
+    #interactively classify input text 
+    answer = 'n'
+    print ("Do you want to try this classifier? (y/n)")
+    answer = input()
+    while answer == 'y':
+        print ("Enter your review:") 
+        input_review = input() 
+        classify_this(classifier, input_review, restaurant_category)
+        print ("Do you want to try this classifier? (y/n)")
+        answer = input()
+
+
 def main():
+    """
+    This classifier determines if a review text is written by 
+    someone who has authentic cuisine experiences by living in the country
+    or from the country. 
+    
+    Although there are some hooks to handle different cuisines, 
+    for the initial phase, reviews for Japanese restaurants are used to do this classification.
+    The classifier along with the training data is looking 
+    for key word/word pattern matches in conjunction with Entity Recognition tag.
+    Phrases such as "lived in Japan" or "I am Japanese" should have a high probability
+    for the authenticity expert.
+    """
+
     load_geo_words()
-    restaurant_category = "Japanese".lower()
+    if len(sys.argv) > 1:
+        restaurant_category = str(sys.argv[1]).lower()
+    else:
+        restaurant_category = DEFAULT_CUISINE
     featuresets = []
     from os import listdir
     from os.path import isfile, join
@@ -45,13 +115,13 @@ def main():
     for f in onlyfiles:
         with open(data_path_exp + f, 'rU', newline='', encoding='utf-8') as csvfile:
             reviews = csv.reader(csvfile, delimiter=',', quotechar='"')
-            featuresets += [(entity_features(raw[1], restaurant_category), "EXPT") for raw in reviews]
+            featuresets += [(entity_features(raw[1], restaurant_category), EXPERT) for raw in reviews]
         csvfile.close()
     onlyfiles = [f for f in listdir(data_path_nonexp) if isfile(join(data_path_nonexp, f))]
     for f in onlyfiles:
         with open(data_path_nonexp + f, 'rU', newline='', encoding='utf-8') as csvfile:
             reviews = csv.reader(csvfile, delimiter=',', quotechar='"')
-            featuresets += [(entity_features(raw[1], restaurant_category), "NOEX") for raw in reviews]
+            featuresets += [(entity_features(raw[1], restaurant_category), NOEXPERT) for raw in reviews]
         csvfile.close()
     random.shuffle(featuresets)
     size = int(len(featuresets) * 0.1)
@@ -59,6 +129,19 @@ def main():
     classifier = nltk.NaiveBayesClassifier.train(train_set)
     print("accuracy:", nltk.classify.accuracy(classifier, test_set))
     classifier.show_most_informative_features(5)
+
+    #interactivly classify 
+    #classify_with_input(classifier, restaurant_category)
+
+    input_review = "This is a great place! I am Japanese and I used to eat this type of food."
+    print ("review text:", input_review)
+    classify_this(classifier, input_review, restaurant_category)
+    input2_review = "I love Japanese food! I can eat sushi everyday."
+    print ("review text:", input2_review)
+    classify_this(classifier, input2_review, restaurant_category)
+    input3_review = "Don't care for the food. Trust me - I used to live in Tokyo."
+    print ("review text:", input3_review)
+    classify_this(classifier, input3_review, restaurant_category)
 
 if __name__ == "__main__":
     main()
